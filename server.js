8cc8b7128623f9404f3d6addce91f82b42286608
livleensinghchani <livleensinghchani@gmail.com>
2025-03-27 12:48:29 +0530

    Push

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ console.log("SERVER 🖥️ STARTED!");
 
 const db = require("./db");
 const express = require("express");
+const path = require('path')
 
 const app = express();
-app.use(express.static("public"));
+app.use(express.static("public/home"));
 
 // Use if data is fetched using Forms
 app.use(express.urlencoded({extended: true})) 
@@ -15,19 +16,10 @@ app.listen(3000, () => {
   console.log("App running on http://localhost:3000");
 })
 
-app.post("/addUser", (req, res) => {
-  const {username} = req.body
-  const lowerUsername = username.toLowerCase();
-  db.insertUser(lowerUsername, (err) => {
-    if (err) {
-      return res.status(500).send("Insert Failed!");
-    }
-    res.send(`USER: ${lowerUsername} ADDED!`);
-  })
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/home/index.html'));
 })
 
-app.get("/getUsers", (req, res) => {
-  db.getUsers((err, rows) => {
-    res.send(rows)
-  })
+app.get("/trans", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/trans/index.html'));
 })
